70901724fc8b5d1298ac5088ce88fee2f0c4911c
LezcanoJorge <yochyoch2015@gmail.com>
2023-11-14 15:00:43 -0300

    registro responsivo

--- sapucaimod/src/app/components/auth/sesion/auth.module.ts
+++ sapucaimod/src/app/components/auth/sesion/auth.module.ts
@@ -5,7 +5,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { authRoutingModule } from './auth.routing';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { SesionComponent } from '../sesion/sesion.component';
+import { SesionComponent } from './sesion.component';
 import { MiApiService } from '../../../services/auth/mi-api.service';
 
 
