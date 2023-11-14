bedc9c7b5cd81b5b286c91343113720ec7f62234
LezcanoJorge <yochyoch2015@gmail.com>
2023-11-13 22:49:17 -0300

    modificar usuario

---
+++ sapucaimod/src/app/components/inicio/auth/modificar/modificar.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../../../services/usuario.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../../../models/usuario.model';
+import { registerForm } from 'interfaces/register-form.intefaces';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-modificar',
+  templateUrl: './modificar.component.html',
+  styleUrls: ['./modificar.component.css'],
+})
+export class ModificarComponent implements OnInit {
+  usuarioId: number = 0;
+  usuarioForm!: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private usuarioService: UsuarioService,
+    private formBuilder: FormBuilder,
+    private router: Router  // Agrega el servicio Router
+  ) {}
+
+  ngOnInit(): void {
+    this.usuarioForm = this.formBuilder.group({
+      id: [null, Validators.required], // Deja este campo en blanco
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [''],
+      // Otros campos del usuario
+    });
+  }
+
+  cargarUsuario(): void {
+    const idIngresado = this.usuarioForm.get('id')?.value;
+    if (idIngresado) {
+      this.usuarioService.obtenerUsuarioPorId(idIngresado).subscribe(
+        (usuario: registerForm) => {
+          this.usuarioForm.patchValue(usuario);
+        },
+        (error: any) => {
+          console.error('Error al cargar el usuario:', error);
+        }
+      );
+    }
+  }
+
+  guardarCambios(): void {
+    if (this.usuarioForm.valid) {
+      const datosModificados = this.usuarioForm.value;
+
+      // Crear un nuevo objeto Usuario con las propiedades necesarias
+      const usuarioConvertido: Usuario = {
+        ID_Usuario: datosModificados.id || 0,
+        Nombre: datosModificados.nombre || '',
+        Email: datosModificados.email || '',
+        Password: datosModificados.password || '',
+        // Otros campos del usuario
+      };
+
+      this.usuarioService.actualizarUsuario(usuarioConvertido.ID_Usuario, usuarioConvertido).subscribe(
+        (respuesta) => {
+          console.log('Usuario actualizado correctamente:', respuesta);
+
+          // Redirigir a la página de dashboard después de la actualización exitosa
+          this.router.navigate(['/dashboard']);  // Ajusta la ruta según tu configuración
+        },
+        (error) => {
+          console.error('Error al actualizar el usuario:', error);
+        }
+      );
+    } else {
+      console.log('El formulario no es válido');
+    }
+  }
+}
